e577d30fb149f9a8e4a5e9ed553ba280049b3dc9
jrichardson78745 <jrichardson@brainwerkz.com>
2017-04-19 21:50:36 -0500

    AddRollbar

--- flask1.py
+++ flask1.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+ROLLBAR = {
+    'access_token': '8968e0219c5342bd96b76b8c1cab8bee',
+    'environment': 'development' if DEBUG else 'production',
+    'branch': 'master',
+    'root': '/absolute/path/to/code/root',
+}
+
 from flask import Flask
 import sys
 import optparse
